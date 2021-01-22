fc9b1eb0fda6e209de4bb8b105db8f4706f771e7
tonyw0527 <tonyw0527@gmail.com>
2021-01-23 07:25:14 +0900

    Updated _document.tsx

--- client/components/basics/Layout.tsx
+++ client/components/basics/Layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import Head from "next/head";
 import Header from "./Header";
 import Copyright from "./Copyright";
 import styled from "styled-components";
@@ -19,14 +18,6 @@ type LayoutProps = {
 
 const Layout = ({ children, onToggleTheme }: LayoutProps) => (
   <Container>
-    <Head>
-      <meta charSet="utf-8" />
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="성경 읽기 사이트입니다." />
-      <title>성경 앱</title>
-    </Head>
-
     <Header onToggleTheme={onToggleTheme} />
     {children}
     <Copyright />
