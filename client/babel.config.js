df9abb37ca09f446083c0f225f0a15598ec73afe
tonyw0527 <tonyw0527@gmail.com>
2021-01-23 04:51:12 +0900

    add babel.config.js

---
+++ client/babel.config.js
@@ -0,0 +1,15 @@
+// {
+//   "presets": ["next/babel"],
+//   "plugins": [["styled-components", { "ssr": true }]]
+// }
+
+const presets = ['next/babel'];
+const plugins = [
+  ['styled-components', { 'ssr': true }],
+]
+
+if(process.env['VERCEL_ENV'] === 'production') {
+  plugins.push(['transform-remove-console'])
+}
+
+module.exports = {presets, plugins};
