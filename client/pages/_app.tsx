5b9bf8d388a0d1d0edf715f2a035ab78d0f1ee12
tonyw0527 <tonyw0527@gmail.com>
2021-01-26 03:43:44 +0900

    Moved some head tags from _app.tsx to _document.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -17,37 +17,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta charSet="utf-8" />
-        <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="성경 읽기 사이트입니다." />
-        <title>성경 앱</title>
-        {/* favicon tags */}
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        {/* OG tags */}
-        <meta property="og:url" content="http://histime.ga" />
-        <meta property="og:title" content="Bible App" />
-        <meta property="og:description" content="아름다운 성경 앱입니다." />
-        <meta
-          property="og:image"
-          content="https://histime.ga/android-chrome-512x512.png"
-        />
       </Head>
       <StoreProvider {...pageProps}>
         <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +26,49 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="manifest" href="/site.webmanifest" />
+
+          <meta name="description" content="성경 읽기 사이트입니다." />
+          <title>성경 앱</title>
+          {/* favicon tags */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          {/* OG tags */}
+          <meta property="og:url" content="http://histime.ga" />
+          <meta property="og:title" content="Bible App" />
+          <meta property="og:description" content="아름다운 성경 앱입니다." />
+          <meta
+            property="og:image"
+            content="https://histime.ga/android-chrome-512x512.png"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
