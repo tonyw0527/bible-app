8dd136450938aa111973b191d77d9ddf2372dda7
tonyw0527 <tonyw0527@gmail.com>
2021-01-26 03:17:54 +0900

    Deleted next.config.js

--- client/next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.VERCEL_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.histime.ga' : '',
-}
