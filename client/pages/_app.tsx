eb22f77a93d05b11183734f9a3f6f04a2c917f46
tonyw0527 <tonyw0527@gmail.com>
2021-01-26 03:03:44 +0900

    Updated _app.tsx - added OG tags

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -18,6 +18,11 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta charSet="utf-8" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="성경 읽기 사이트입니다." />
+        <title>성경 앱</title>
+        {/* favicon tags */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -35,10 +40,14 @@ function App({ Component, pageProps }: AppProps) {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="성경 읽기 사이트입니다." />
-        <title>성경 앱</title>
+        {/* OG tags */}
+        <meta property="og:url" content="http://histime.ga" />
+        <meta property="og:title" content="Bible App" />
+        <meta property="og:description" content="아름다운 성경 앱입니다." />
+        <meta
+          property="og:image"
+          content="https://cdn.histime.ga/android-chrome-512x512.png"
+        />
       </Head>
       <StoreProvider {...pageProps}>
         <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
