ba9c4fb025b3c7cba7a9cb0a20dce22aee4cec66
tonyw0527 <tonyw0527@gmail.com>
2021-01-26 01:22:41 +0900

    Added favicon

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -18,7 +18,24 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="성경 읽기 사이트입니다." />
         <title>성경 앱</title>
