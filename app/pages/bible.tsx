d97e6f27ddb9e3510961deecddfeb0673cd6a77c
tonyw0527 <tonyw0527@gmail.com>
2021-02-05 00:15:26 +0900

    Add basic auth redirection in bible.tsx

--- app/pages/bible.tsx
+++ app/pages/bible.tsx
@@ -1,7 +1,17 @@
 import Head from "next/head";
 import BibleComponent from "../components/bible";
+import { useRouter } from "next/router";
+import { useStore } from "../stores/StoreProvider";
 
 export default function Bible() {
+  const router = useRouter();
+  const store = useStore();
+
+  if (!store.userStore.isAuth) {
+    alert("초대코드를 입력하세요");
+    router.push("/");
+  }
+
   return (
     <>
       <Head>
